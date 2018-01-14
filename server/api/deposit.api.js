5cbcea666009efd27785e71d34c286a8140d2ba7
joachimprinzbach <joachim.prinzbach@gmail.com>
2018-01-14 17:38:02 +0100

    minor refactoring

--- server/api/deposit.api.js
+++ server/api/deposit.api.js
@@ -3,10 +3,12 @@ const binance = require('./external/binance/binance.api');
 const pricing = require('./external/cryptocompare/pricing.api');
 
 const currency = 'EUR';
+const sum = (a, b) => a + b;
 
 const getDeposits = () => {
     return binance.getAllDeposits().then(result => {
         const deposits = result.depositList;
+        console.log(deposits);
         const priceInfoPromises = deposits.map(deposit => {
             const depositTimeInSeconds = Math.round(deposit.insertTime / 1000);
             const cachedPrice = pricing.getCachedPrice(deposit.asset, currency, depositTimeInSeconds);
@@ -16,22 +18,21 @@ const getDeposits = () => {
             return pricing.getHistoricalPrice(deposit.asset, currency, depositTimeInSeconds)
         });
         return Promise.all(priceInfoPromises).then(res => {
-            const items = priceInfoPromises.length;
-            for (let i = 0; i < items; i++) {
-                const priceInfo = res[i][deposits[i].asset];
+            for (let i = 0; i < priceInfoPromises.length; i++) {
+                const priceInfo = res[i];
+                const assetPrice = priceInfo[deposits[i].asset];
                 const depositTimeInSeconds = Math.round(deposits[i].insertTime / 1000);
-                if (priceInfo) {
-                    const historicalPrice = priceInfo[currency];
+                if (assetPrice) {
+                    const historicalPrice = assetPrice[currency];
                     const cachedPrice = pricing.getCachedPrice(deposits[i].asset, currency, depositTimeInSeconds);
                     if(!cachedPrice) {
-                        pricing.cachePrice(deposits[i].asset, currency, depositTimeInSeconds, res[i]);
+                        pricing.cachePrice(deposits[i].asset, currency, depositTimeInSeconds, priceInfo);
                     }
                     deposits[i].transactionValue = historicalPrice * deposits[i].amount;
                 } else {
                     console.error(`No historical price found for ${deposits[i].asset} at ${depositTimeInSeconds}`);
                 }
             }
-            const sum = (a, b) => a + b;
             return {deposits: deposits.map(deposit => deposit.transactionValue).reduce(sum)};
         });
     });
@@ -39,8 +40,7 @@ const getDeposits = () => {
 
 module.exports = {
     registerEndpoints(app) {
-        app.get('/api/deposit',
-            (req, res) =>
+        app.get('/api/deposit', (req, res) =>
                 getDeposits()
                     .then(deposits => res.json(deposits))
                     .catch((err) => {
